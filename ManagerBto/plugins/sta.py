71d295feaacdea42341cb03c3507903b7f8e8e38
Rihad <74430658+Mensimov@users.noreply.github.com>
2021-04-01 22:43:02 +0400

    Update sta.py

--- ManagerBto/plugins/sta.py
+++ ManagerBto/plugins/sta.py
@@ -1,42 +1,17 @@
 from telethon.tl.custom import Button
 from telethon import events
-from .. import klent, ADMIN, KANAL
+from .. import klent, ADMIN, KANAL, add_to_db
 
 # admine gonderilenler
 @klent.on(events.NewMessage)
 async def _(event):
-    if event.is_private:
-        # media gelibse
-        if event.media:
-            x = event.media
-            aydi = event.sender_id
-            if aydi == int(ADMIN):
-                return
-            # medianin caption varsa
-            if event.text:
-                gonderen = f'Göndərən: [{event.sender.first_name}](tg://user?id={aydi})\nID: `{aydi}`'
-                filee = await event.client.send_file(ADMIN,x,caption=event.text,buttons=[
-                        Button.inline('Kanala göndər',b'post')
-                        ])
-                await event.client.send_message(ADMIN,gonderen,reply_to=filee)
-
-            # caption yoxsa
+    if event.sender_id != ADMIN:
+        if event.is_private:
+            if event.media:
+                msgtoadmin = await event.client.send_file(ADMIN,event.media,caption=event.text,buttons=[Button.inline('Kanalda paylaş', b'post')])
             else:
-                gonderen = f'Göndərən: [{event.sender.first_name}](tg://user?id={aydi})\nID: `{aydi}`'
-                filee = await event.client.send_file(ADMIN,x,buttons=[
-                    Button.inline('Kanala göndər',b'post')])
-                await event.client.send_message(ADMIN,gonderen,reply_to=filee)
-        # yazi gelibse
-        else:
-            y = event.text
-            ad = event.sender.first_name
-            aydii = event.sender_id
-            if aydii == int(ADMIN):
-                return
-            msgtoadmin = await event.client.send_message(ADMIN,y)
-            await event.client.send_message(ADMIN,f'Yazan: [{ad}](tg://user?id=' + str(aydii) + ')' + '\nID: ' + '`' + str(aydii) + '`',reply_to=msgtoadmin)
-    else:
-        pass
+                msgtoadmin = await event.client.send_message(ADMIN,event.text)
+            add_to_db(msgtoadmin.id,event.sender_id,event.sender.first_name)
 @klent.on(events.CallbackQuery(data=b'post'))
 async def paylas(event):
     mesaj = await event.get_message()
