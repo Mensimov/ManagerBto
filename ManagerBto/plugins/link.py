69c976e0ddfb953aed78a7cb36be663dbd1622b9
Rihad <74430658+Mensimov@users.noreply.github.com>
2021-04-01 23:01:16 +0400

    Create link.py

---
+++ ManagerBto/plugins/link.py
@@ -0,0 +1,15 @@
+from .. import klent, ADMIN, con, cursor
+from telethon import events
+
+@klent.on(events.NewMessage(pattern='/link'))
+async def link(event):
+	if event.sender_id == ADMIN:
+		rep = await event.get_reply_message()
+		if not rep:
+			await event.reply('Bir mesaja yanıt ver')
+		try:
+			a = cursor.execute(f"SELECT ad, userid FROM users WHERE msgid = {rep.id}")
+			mesaj = a.fetchall()
+			await event.reply(f'İStifadəçi: [{mesaj[0][0]}](tg://user?id={mesaj[0][1]})\nID: `{mesaj[0][1]}`')
+		except:
+			await event.reply('Mesaj DataBase- də tapılmnadı')
