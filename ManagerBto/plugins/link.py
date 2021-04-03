63744116ccdd15923c02394d59139785e410ee87
Rihad <74430658+Mensimov@users.noreply.github.com>
2021-04-03 22:53:42 +0400

    Update link.py

--- ManagerBto/plugins/link.py
+++ ManagerBto/plugins/link.py
@@ -6,11 +6,11 @@ async def link(event):
 	if event.sender_id == ADMIN:
 		rep = await event.get_reply_message()
 		if not rep:
-			await event.reply('Bir mesaja yanıt ver')
-                        return
+                    await event.reply('Bir mesaja yanıt ver')
+                    return
 		try:
-			a = cursor.execute(f"SELECT ad, userid FROM users WHERE msgid = {rep.id}")
-			mesaj = a.fetchall()
-			await event.reply(f'İStifadəçi: [{mesaj[0][0]}](tg://user?id={mesaj[0][1]})\nID: `{mesaj[0][1]}`')
+	            a = cursor.execute(f"SELECT ad, userid FROM users WHERE msgid = {rep.id}")
+		    mesaj = a.fetchall()
+		    await event.reply(f'İStifadəçi: [{mesaj[0][0]}](tg://user?id={mesaj[0][1]})\nID: `{mesaj[0][1]}`')
 		except:
-			await event.reply('Mesaj DataBase- də tapılmnadı')
+		    await event.reply('Mesaj DataBase- də tapılmnadı')
