1ce585ae7a254718e73a60853ec83480abee5e1a
Rihad <74430658+Mensimov@users.noreply.github.com>
2021-04-03 23:36:58 +0400

    Update link.py

--- ManagerBto/plugins/link.py
+++ ManagerBto/plugins/link.py
@@ -3,14 +3,14 @@ from telethon import events
 
 @klent.on(events.NewMessage(pattern='/link'))
 async def link(event):
-	if event.sender_id == ADMIN:
-		rep = await event.get_reply_message()
-		if not rep:
-                    await event.reply('Bir mesaja yanıt ver')
-                    return
-		try:
-                    a = cursor.execute(f"SELECT ad, userid FROM users WHERE msgid = {rep.id}")
-                    mesaj = a.fetchall()
-                    await event.reply(f'İStifadəçi: [{mesaj[0][0]}](tg://user?id={mesaj[0][1]})')
-                except:
-                    await event.reply('Mesaj DataBase də tapılmadı')
+    if event.sender_id == ADMIN:
+        rep = await event.get_reply_message()
+    if not rep:
+        await event.reply('Bir mesaja yanıt ver')
+        return
+    try:
+        a = cursor.execute(f"SELECT ad, userid FROM users WHERE msgid = {rep.id}")
+        mesaj = a.fetchall()
+        await event.reply(f'İStifadəçi: [{mesaj[0][0]}](tg://user?id={mesaj[0][1]})')
+    except:
+        await event.reply('Mesaj DataBase də tapılmadı')
